bb6cf4f55b81e749c0864383c07f7843120320bc
remfuji <richi.work@gmail.com>
2023-07-26 18:30:12 +0200

    Playground

---
+++ Playground/header_javascript/script.js
@@ -0,0 +1,50 @@
+const hamburger = document.getElementById('hamburger');
+const menu = document.getElementById('menu');
+const tendina = document.getElementById('tendina-menu');
+
+function updateMenuDisplay() {
+    if (window.innerWidth < 768) {
+        menu.style.display = 'none';
+        hamburger.style.display = 'flex';
+
+    } else {
+        menu.style.display = 'flex';
+        hamburger.style.display = 'none';
+        tendina.style.display = 'none';
+    }
+}
+
+window.addEventListener('resize', () => {
+    updateMenuDisplay();
+});
+// window.addEventListener('resize', () => {
+//     if (window.innerWidth < 768) {
+//       menu.style.display = 'none';
+//       hamburger.style.display='none';
+//     } else {
+//         menu.style.display = 'flex';
+//         hamburger.style.display = 'none';
+//     }
+//   });
+//   window.addEventListener('resize', () => {
+//     if (window.innerWidth > 767) {
+//       menu.style.display = 'flex';
+//       hamburger.style.display='none';
+//     } else {
+//         menu.style.display ='none';
+//         hamburger.style.display = 'flex';
+//     }
+//   });
+
+
+hamburger.addEventListener('click', () => {
+    if (tendina.style.display === 'none') {
+        tendina.style.display = 'flex';
+
+    } else {
+        tendina.style.display = 'none';
+    }
+});
+
+
+
